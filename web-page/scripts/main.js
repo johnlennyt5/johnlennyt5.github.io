8ab7471fd90339df951d1359f8c38b07a8298015
johnlennyt5 <127471152+johnlennyt5@users.noreply.github.com>
2023-03-10 19:11:54 -0500

    Update main.js

--- web-page/scripts/main.js
+++ web-page/scripts/main.js
@@ -4,7 +4,7 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
   if(mySrc === 'https://upload.wikimedia.org/wikipedia/commons/5/56/Kobe_Bryant_2014.jpg') {
-    myImage.setAttribute ('src','https://www.foodista.com/sites/default/files/styles/featured/public/field/image/kobe%20bryant_0.jpg');
+    myImage.setAttribute ('src','https://upload.wikimedia.org/wikipedia/commons/e/e0/Lipofsky_Kobe.jpg');
   } else {
     myImage.setAttribute ('src','https://upload.wikimedia.org/wikipedia/commons/5/56/Kobe_Bryant_2014.jpg');
   }
